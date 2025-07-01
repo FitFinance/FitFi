7f856ada8dbaab26ebdd6ed3e29d78a49e93a9b2
Arnav Chhabra <65032719+Halleys123@users.noreply.github.com>
2025-07-02 02:12:11 +0530

    fix
    removed nativewind as that was not working will work with normal styles

--- fitfi-mobile-app/app/index.tsx
+++ fitfi-mobile-app/app/index.tsx
@@ -1,15 +1,24 @@
-import { Text, View } from "react-native";
+import '../global.css';
+import { Text, View, StyleSheet } from 'react-native';
 
 export default function Index() {
+  verifyInstallation();
   return (
-    <View
-      style={{
-        flex: 1,
-        justifyContent: "center",
-        alignItems: "center",
-      }}
-    >
-      <Text>Edit app/index.tsx to edit this screen.</Text>
+    <View style={styles.container}>
+      <Text style={styles.text}>This page is big.</Text>
     </View>
   );
 }
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    alignItems: 'center',
+    justifyContent: 'center',
+    backgroundColor: '#f0f0f0',
+  },
+  text: {
+    fontSize: 20,
+    color: '#333',
+  },
+});
